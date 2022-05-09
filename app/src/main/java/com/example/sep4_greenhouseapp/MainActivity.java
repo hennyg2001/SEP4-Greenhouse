8f93af4ddba31055925cb8f303aaa18d098c898e
hennyg2001 <henrikmiki@gmail.com>
2022-05-09 10:41:35 +0200

    New Firebase Classes 2

--- app/src/main/java/com/example/sep4_greenhouseapp/MainActivity.java
+++ app/src/main/java/com/example/sep4_greenhouseapp/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.sep4_greenhouseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView loginSuccess;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        // initialising all views through id defined above
+        loginSuccess = findViewById(R.id.ls);
+        loginSuccess.setText("Welcome!");
     }
 
     @Override
